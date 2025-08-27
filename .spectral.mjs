5b4c8b33d7a2009f7d580419aa1868b62ac3b5d4
Aaron Sachs <aaron@sachshaus.net>
2025-08-27 15:55:03 -0400

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI5MTA5Mw/spectral.js?branch_slug=main&token=7dab2ce7-4ef9-4d5a-a2ab-cf704c2bfef3";
+export default { extends: ruleset };
